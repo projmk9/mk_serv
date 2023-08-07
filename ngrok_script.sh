009b2cfd78c979a45847640ebebd15fd97e2c18f
projmk9 <projmk9@gmail.com>
2023-08-08 01:45:03 +0700

    update

--- ngrok_script.sh
+++ ngrok_script.sh
@@ -7,17 +7,18 @@ PATH_PROJ="~/mk_serv"
 #cd $PATH_PROJ
 
 killall ngrok
+rm ngrok_json.txt
 
 #run ngrok service
-screen -d -m  ngrok tcp $SERVER:22 
+screen -d -m ngrok tcp $SERVER:22 
 
-sleep 6
+sleep 12
 
 #run curl service > ngrok_json.txt
 curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt  
 
 
-sleep 6
+sleep 12
 
 
 #cd $PATH_PROJ

15810fb70b4695f3b90140ede1ec5409b181b8ce
projmk9 <projmk9@gmail.com>
2023-08-08 01:05:55 +0700

    update

--- ngrok_script.sh
+++ ngrok_script.sh
@@ -4,7 +4,7 @@ SERVER="127.0.0.1"
 PATH_PROJ="~/mk_serv"
  
 # cd to path
-cd $PATH_PROJ
+#cd $PATH_PROJ
 
 
 
@@ -14,13 +14,13 @@ screen -d -m  ngrok tcp $SERVER:22
 sleep 6
 
 #run curl service > ngrok_json.txt
-screen -d -m curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt  
+curl 127.0.0.1:4040/api/tunnels > ngrok_json.txt  
 
 
 sleep 6
 
 
-cd $PATH_PROJ
+#cd $PATH_PROJ
 
 git config --global user.email "projmk9@gmail.com"
 git config --global user.name "projmk9"
